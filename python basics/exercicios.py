24cba3daeeed573283d3075f7eb25e0c0b83842c
Jonanthan <jonanthan.dom@gmail.com>
2025-03-27 15:54:23 -0300

    atualizando

--- python basics/exercicios.py
+++ python basics/exercicios.py
@@ -684,6 +684,3 @@ sexo = input('Digite seu sexo (M/F): ').strip().upper()[0]
 while sexo not in 'MF':
     sexo = input('Dados inválidos, por favor, informe seu sexo (M/F): ').strip().upper()[0]    
 print('Sexo {} registrado com sucesso!'.format(sexo))
-
-
-print('teste')
